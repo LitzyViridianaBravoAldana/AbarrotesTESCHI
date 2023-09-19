371ae9a5c420cdd1e0e13841fb055d0ecdf64c31
LitzyViridianaBravoAldana <viri30059@example.com>
2023-09-19 14:45:04 -0600

    primera fk

--- APITESCHI/api/models.py
+++ APITESCHI/api/models.py
@@ -14,4 +14,15 @@ class Categoria(models.Model):
     Descripcion = models.TextField(db_column='Descripcion')
     class Meta:
         db_table='Categorias'
-
+class Pais(models.Model):
+    Id_Pais = models.AutoField(primary_key=True,db_column='Id_Pais')
+    Descripcion = models.TextField(db_column='Descripcion')
+    Codigo_Pais = models.TextField(db_column='Codigo_Pais')
+    class Meta:
+        db_table='Paises'
+class Idioma(models.Model):
+    Id_Idioma = models.AutoField(primary_key=True,db_column='Id_Idioma')
+    Descripcion = models.TextField(db_column='Descripcion')
+    Codigo_Idioma = models.TextField(db_column='Codigo_Idioma')
+    class Meta:
+        db_table='Idiomas'
