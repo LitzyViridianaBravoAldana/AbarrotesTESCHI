983e353d877f2e663b484bfa8cde22d165c9c607
LitzyViridianaBravoAldana <viri30059@example.com>
2023-09-19 14:29:00 -0600

    Base de datos Inicio prueba

--- APITESCHI/api/models.py
+++ APITESCHI/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    id_Usuarios = models.AutoField(primary_key=True,db_column='Id_Usuario')
+    Nombre_Usuario = models.TextField(db_column='Nombre_Usuario')
+    Contrasena = models.TextField(db_column='Contrasena')
+    Nombre = models.TextField(db_column='Nombre')
+    Correo = models.EmailField(db_column='Correo')
+    class Meta:
+        db_table='Usuarios'
+class Categoria(models.Model):
+    id_Categoria = models.AutoField(primary_key=True,db_column='Id_Categoria')
+    Descripcion = models.TextField(db_column='Descripcion')
+    class Meta:
+        db_table='Categorias'
+class Pais(models.Model):
+    Id_Pais = models.AutoField(primary_key=True,db_column='Id_Pais')
+    Descripcion = models.TextField(db_column='Descripcion')
+    Codigo_Idioma = models.TextField(db_column='Codigo_Idioma')
+    class Meta:
+        db_table='Paises'
