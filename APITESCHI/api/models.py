c3288d6fa769e8afcf15ba51b18c9a147c5d434e
LitzyViridianaBravoAldana <viri30059@example.com>
2023-09-19 14:57:56 -0600

    CORRECTO

--- APITESCHI/api/models.py
+++ APITESCHI/api/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
 # Create your models here.
-
+class Usuario(models.Model):
+    id_Usuarios = models.AutoField(primary_key=True,db_column='Id_Usuario')
+    Nombre_Usuario = models.TextField(db_column='Nombre_Usuario')
+    Contrasena = models.TextField(db_column='Contrasena')
+    Nombre = models.TextField(db_column='Nombre')
+    Apellido_Pa = models.CharField(max_length=20,db_column='Apellido_P')
+    Apellido_Ma = models.CharField(max_length=20,db_column='Apellido_M')
+    Telefono = models.IntegerField(max_length=10,db_column='Telefono')
+    Correo = models.EmailField(db_column='Correo')
+    class Meta:
+        db_table='Usuarios'
 class Categoria(models.Model):
     id_Categoria = models.AutoField(primary_key=True,db_column='Id_Categoria')
     Descripcion = models.TextField(db_column='Descripcion')
