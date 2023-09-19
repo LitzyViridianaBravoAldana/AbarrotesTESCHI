c1ac1933abdd440c2273b6c694d9dc20aa13a892
LitzyViridianaBravoAldana <viri30059@example.com>
2023-09-19 14:38:37 -0600

    Base hasta idiomas

--- APITESCHI/api/models.py
+++ APITESCHI/api/models.py
@@ -14,9 +14,4 @@ class Categoria(models.Model):
     Descripcion = models.TextField(db_column='Descripcion')
     class Meta:
         db_table='Categorias'
-class Pais(models.Model):
-    Id_Pais = models.AutoField(primary_key=True,db_column='Id_Pais')
-    Descripcion = models.TextField(db_column='Descripcion')
-    Codigo_Idioma = models.TextField(db_column='Codigo_Idioma')
-    class Meta:
-        db_table='Paises'
+
