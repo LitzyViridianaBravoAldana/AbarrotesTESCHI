20d3ef0854025a28116d937e7ec9482162bd49a0
LitzyViridianaBravoAldana <viri30059@example.com>
2023-09-20 11:55:31 -0600

    no funciona pero tiene algo

--- APITESCHI/APITESCHI/urls.py
+++ APITESCHI/APITESCHI/urls.py
@@ -1,16 +1,24 @@
 from django.urls import path
 from api.views import sign_in, sign_up, index_9
+#from tasks import views
 
 urlpatterns = [
+    # Ruta para la página index_9
+    #path('index_9.html', index_9.as_view(), name='index_9'),
+    path('index_9.html/', index_9.as_view(), name='index_9_with_slash'),
+
+
+    # Ruta para la URL raíz (puede ser una de las páginas anteriores)
+    #path('', index_9.as_view(), name='index_9_root'),
+
     # Ruta para la página de inicio de sesión
-    path('sign_in.html', sign_in.as_view(), name='sign_in'),
+    path('sign_in/', sign_in.as_view(), name='sign_in'),
     
     # Ruta para la página de registro
-    path('sign_up.html', sign_up.as_view(), name='sign_up'),
+    path('sign_up/', sign_up.as_view(), name='sign_up'),
+    #path('sign_up.html', views.sign_up, name='sign_up'),
     
-    # Ruta para la página index_9
-    path('index_9.html', index_9.as_view(), name='index_9'),
+
     
-    # Ruta para la URL raíz (puede ser una de las páginas anteriores)
-    path('', index_9.as_view(), name='index_9_root'),
+
 ]
--- APITESCHI/api/views.py
+++ APITESCHI/api/views.py
@@ -1,17 +1,39 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from rest_framework.views import APIView
 # Create your views here.
 
 
 class sign_in(APIView):
-    template_name="sign_in"
+    template_name="sign_in.html"
     def get(self,request):
         return render(request,self.template_name)
+    
+
+#def sign_up(request):
+ #   if request.method == 'GET':
+  #      print('viendo formulario')
+   # else:
+    #    print(request.POST)
+     #   print('mandando formulario')
+#
+ #   return render(request, 'sign_up.')
     
 class sign_up(APIView):
     template_name="sign_up.html"
     def get(self,request):
-        return render(request,self.template_name)
+        
+        if request.method == 'GET':
+            print('Viendo formulario')
+        else:
+            print(request.POST)
+            print('mandando datos')
+
+
+
+        return render(request,'sign_up.html', {
+            'form': UserCreationForm
+        })
     
 class index_9(APIView):
     template_name="index_9.html"
