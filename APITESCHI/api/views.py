538ffce2deeaa189d497ac1ce2e39bdfa6e90610
LitzyViridianaBravoAldana <viri30059@example.com>
2023-09-20 12:13:56 -0600

    no se que hice

--- APITESCHI/api/views.py
+++ APITESCHI/api/views.py
@@ -1,41 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from rest_framework.views import APIView
-# Create your views here.
 
+# Importa tus vistas aquí
 
 class sign_in(APIView):
-    template_name="sign_in.html"
-    def get(self,request):
-        return render(request,self.template_name)
-    
-
-#def sign_up(request):
- #   if request.method == 'GET':
-  #      print('viendo formulario')
-   # else:
-    #    print(request.POST)
-     #   print('mandando formulario')
-#
- #   return render(request, 'sign_up.')
+    template_name='sign_in.html'
+    def get(Self,request):
+        return render(request.self.template_name)
     
 class sign_up(APIView):
-    template_name="sign_up.html"
-    def get(self,request):
-        
-        if request.method == 'GET':
-            print('Viendo formulario')
-        else:
-            print(request.POST)
-            print('mandando datos')
-
-
-
-        return render(request,'sign_up.html', {
-            'form': UserCreationForm
-        })
+    template_name='sign_up.html'
+    def get(Self,request):
+        return render(request.self.template_name)
     
 class index_9(APIView):
-    template_name="index_9.html"
-    def get(self,request):
-        return render(request,self.template_name)
+    template_name='index_9.html'
+    def get(Self,request):
+        return render(request.self.template_name)
