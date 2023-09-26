50bb146bb8be57e354f070a4a2a6845ecf2981cb
LitzyViridianaBravoAldana <viri30059@example.com>
2023-09-26 15:52:55 -0600

    Base de datos con navegacion completa

--- APITESCHI/api/models.py
+++ APITESCHI/api/models.py
@@ -3,20 +3,20 @@ from django.db import models
 # Create your models here.
 class Usuario(models.Model):
     id_Usuarios = models.AutoField(primary_key=True,db_column='Id_Usuario')
-    Nombre_Usuario = models.TextField(db_column='Nombre_Usuario')
+    Nombre_Usuario = models.TextField(unique=True,db_column='Nombre_Usuario')
     Contrasena = models.TextField(db_column='Contrasena')
     Nombre = models.TextField(db_column='Nombre')
     Apellido_Pa = models.CharField(max_length=20,db_column='Apellido_P')
     Apellido_Ma = models.CharField(max_length=20,db_column='Apellido_M')
     Telefono = models.IntegerField(db_column='Telefono')
-    Correo = models.EmailField(db_column='Correo')
+    Correo = models.EmailField(unique=True,db_column='Correo')
     class Meta:
         db_table='Usuarios'
-class Categoria(models.Model):
-    id_Categoria = models.AutoField(primary_key=True,db_column='Id_Categoria')
-    Descripcion = models.TextField(db_column='Descripcion')
+class Genero(models.Model):
+    id_Genero = models.AutoField(primary_key=True,db_column='Id_Categoria')
+    Descripcion = models.TextField(unique=True,db_column='Descripcion')
     class Meta:
-        db_table='Categorias'
+        db_table='Generos'
 class Pais(models.Model):
     Id_Pais = models.AutoField(primary_key=True,db_column='Id_Pais')
     Descripcion = models.TextField(db_column='Descripcion')
@@ -25,7 +25,7 @@ class Pais(models.Model):
         db_table='Paises'
 class Idioma(models.Model):
     Id_Idioma = models.AutoField(primary_key=True,db_column='Id_Idioma')
-    Descripcion = models.TextField(db_column='Descripcion')
+    Descripcion = models.TextField(unique=True,db_column='Descripcion')
     Codigo_Idioma = models.TextField(db_column='Codigo_Idioma')
     class Meta:
         db_table='Idiomas'
@@ -48,7 +48,7 @@ class Autor(models.Model):
 class Libro(models.Model):
     Id_Libro = models.AutoField(primary_key=True,db_column='Id_Libro')
     Fk_Id_Autor = models.ForeignKey(Autor,on_delete=models.CASCADE,default=1,db_column='fk_id_Autor')
-    Fk_Id_Genero = models.ForeignKey(Categoria,on_delete=models.CASCADE,default=1,db_column='fk_id_Genero')
+    Fk_Id_Genero = models.ForeignKey(Genero,on_delete=models.CASCADE,default=1,db_column='fk_id_Genero')
     Fk_Id_Editorial = models.ForeignKey(Editorial,on_delete=models.CASCADE,default=1,db_column='fk_id_Editorial')
     Fk_Id_Idioma= models.ForeignKey(Idioma,on_delete=models.CASCADE,default=1,db_column='fk_id_Idioma')
     Fecha_Publicacion = models.DateField(db_column='Fecha_Registro')
@@ -85,3 +85,17 @@ class Libro_has_Autor(models.Model):
     Fk_Id_Autor = models.ForeignKey(Autor,on_delete=models.CASCADE,db_column='Fk_Id_Autor')
     class Meta:
         db_table='Libro_has_Autor'
+
+class Libro_has_Idioma(models.Model):
+    Id_Libro_has_Idioma = models.AutoField(primary_key=True,db_column='Id_Libro_has_Idioma')
+    Fk_Id_Libro = models.ForeignKey(Libro,on_delete=models.CASCADE,db_column='Fk_Id_Libro')
+    Fk_Id_Idioma = models.ForeignKey(Idioma,on_delete=models.CASCADE,db_column='Fk_Id_Idioma')
+    class Meta:
+        db_table='Libro_has_Idioma'
+
+class Libro_has_Genero(models.Model):
+    Id_Libro_has_Genero = models.AutoField(primary_key=True,db_column='Id_Libro_has_Genero')
+    Fk_Id_Libro = models.ForeignKey(Libro,on_delete=models.CASCADE,db_column='Fk_Id_Libro')
+    Fk_Id_Genero = models.ForeignKey(Genero,on_delete=models.CASCADE,db_column='Fk_Id_Genero')
+    class Meta:
+        db_table='Libro_has_Genero'
